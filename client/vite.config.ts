2828a253bbbc09e072d77938b411ac999a8956a5
Chamindu24 <chamindus.22@cse.mrt.ac.lk>
2026-07-15 12:38:02 +0530

    feat: add vercel.json rewrite rule to support client-side routing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig,loadEnv  } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
-  console.log(env);
-  console.log("API:", env.VITE_API_URL);
 
   return {
     plugins: [react()],
